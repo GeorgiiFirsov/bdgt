b5e9674c6b2defe19ce09cc1c1ed0f093dab6be5
GeorgyFirsov <gfirsov007@gmail.com>
2023-08-25 23:16:12 +0000

    chore: error docs
    Signed-off-by: GeorgyFirsov <gfirsov007@gmail.com>

--- src/errors.rs
+++ src/errors.rs
@@ -1,5 +1,5 @@
-///
+/// Command line arguments parsing error description.
 pub(crate) const PARSE_ERROR: &str = "Cannot properly parse supplied arguments";
 
-///
+/// Error, that is occurred when initialization is performed more than once.
 pub(crate) const ALREADY_INITIALIZED: &str = "Application's storage is already initialized";
