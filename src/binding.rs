d6c47fc3584569d23dd6e49a268f53507868a8da
Georgy Firsov <gfirsov007@gmail.com>
2023-08-26 21:11:58 +0000

    chore: budget opening function

--- src/binding.rs
+++ src/binding.rs
@@ -14,11 +14,14 @@ type Engine = GpgCryptoEngine;
 /// Corresponding key identifier type alias.
 type KeyId = <GpgCryptoEngine as CryptoEngine>::KeyId;
 
+/// Storage type alias for quick storage changes.
+type Storage = DbStorage;
+
 
 /// Performs initialization of the storage.
 /// 
 /// * `key_id` - identifier of a key used to protect data
-pub(crate) fn initialize(key_id: &str) -> Result<Budget<Engine, DbStorage>> {
+pub(crate) fn initialize(key_id: &str) -> Result<Budget<Engine, Storage>> {
     //
     // Check for storage existence
     //
@@ -42,7 +45,36 @@ pub(crate) fn initialize(key_id: &str) -> Result<Budget<Engine, DbStorage>> {
     // now I can create storage
     //
 
-    let config = Config::<Engine>::create(&loc, &id)?;
-    let storage = DbStorage::create(&loc)?;
+    let config = Config::create(&loc, &id)?;
+    let storage = Storage::create(&loc)?;
+    
     Budget::new(engine, storage, config)
+}
+
+
+/// Opens budget manager with performing of some checks.
+pub(crate) fn open_budget() -> Result<Budget<Engine, Storage>> {
+    let loc = ensure_location()?;
+
+    //
+    // Storage root exists here, now I can just open everything
+    //
+
+    let engine = Engine::new()?;
+    let config = Config::open(&loc)?;
+    let storage = Storage::open(&loc)?;
+
+    Budget::new(engine, storage, config)
+}
+
+
+fn ensure_location() -> Result<HomeLocation> {
+    let loc = HomeLocation::new();
+    if loc.exists() {
+        Ok(loc)
+    }
+    else {
+        Err(Error::from_message_with_extra(
+            errors::NOT_INITIALIZED, loc.root().to_str().unwrap()))
+    }
 }
