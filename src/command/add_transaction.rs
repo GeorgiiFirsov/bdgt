2a2d8b89ea8aa63ab192bdb45c7f273e7cc93c31
GeorgyFirsov <gfirsov007@gmail.com>
2023-08-31 07:19:52 +0000

    chore: 'add' logic
    Signed-off-by: GeorgyFirsov <gfirsov007@gmail.com>

--- src/command/add_transaction.rs
+++ src/command/add_transaction.rs
@@ -1,4 +1,5 @@
-use libbdgt::error::Result;
+use libbdgt::error::{Result, Error};
+use libbdgt::storage::{Id, Transaction};
 
 use super::command::{Command, CommandInternal};
 use crate::binding;
@@ -23,7 +24,16 @@ impl Command for AddTransaction {
         let (multi, full) = Self::parse_args(matches)?;
         let budget = binding::open_budget()?;
 
-        // TODO
+        while {
+            Self::input_transaction(full)
+                .and_then(|(account, transaction)| budget.add_transaction(account, transaction))?;
+
+            //
+            // If multiple transactions requested, then ask if one needs to add another one
+            //
+
+            multi && Self::needs_another_transaction()
+        } { /* Intentionally empty */ } 
 
         Ok(())
     }
@@ -38,5 +48,18 @@ impl CommandInternal for AddTransaction {
         let full = Self::get_one(matches, "full")?;
 
         Ok((multi, full))
+    }
+}
+
+
+impl AddTransaction {
+    fn input_transaction(full: bool) -> Result<(Id, Transaction)> {
+        // TODO
+        Err(Error::from_message("not implemented"))
+    }
+
+    fn needs_another_transaction() -> bool {
+        // TODO
+        false
     }
 }
