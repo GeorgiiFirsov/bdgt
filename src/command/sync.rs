9e9928286754e41bc18e7e9451ac0c621e40d05a
Georgy Firsov <gfirsov007@gmail.com>
2024-02-04 14:32:39 +0000

    chore: libbdgt updated

--- src/command/sync.rs
+++ src/command/sync.rs
@@ -5,6 +5,16 @@ use crate::error::{Error, Result};
 use crate::binding;
 
 
+/// Structure with command parameters.
+pub(crate) struct Parameters  {
+    /// New remote URL (if any).
+    remote: Option<String>,
+
+    /// Don't sync after URL change.
+    no_sync: bool,
+}
+
+
 /// Command for showing application's info.
 pub(crate) struct Sync;
 
@@ -23,9 +33,38 @@ impl Command for Sync {
             )
     }
 
-    fn invoke(_matches: &clap::ArgMatches) -> Result<()> {
+    fn invoke(matches: &clap::ArgMatches) -> Result<()> {
+        let parameters = Self::parse_args(matches)?;
         let budget = binding::open_budget()?;
         
+        if let Some(remote) = parameters.remote {
+            budget.set_remote_url(&remote)?
+        }
+
+        if !parameters.no_sync {
+            Self::perform_sync(&budget)?;
+        }
+
+        Ok(())
+    }
+}
+
+
+impl CommandInternal for Sync {
+    type ParsedArgs = Parameters;
+
+    fn parse_args(matches: &clap::ArgMatches) -> Result<Self::ParsedArgs> {
+        Ok(Parameters{
+            remote: Self::get_one_opt(matches, "set-remote"),
+            no_sync: Self::get_one(matches, "no-sync")
+                .unwrap_or(false),
+        })
+    }
+}
+
+
+impl Sync {
+    fn perform_sync(budget: &binding::Budget) -> Result<()> {
         let mut input = pinentry::PassphraseInput::with_default_binary()
             .ok_or(Error::from_message("No pinentry binary found"))?;
         
@@ -37,16 +76,6 @@ impl Command for Sync {
         
         budget.perform_sync(passphrase.expose_secret().as_bytes())?;
 
-        Ok(())
-    }
-}
-
-
-impl CommandInternal for Sync {
-    type ParsedArgs = ();
-
-    fn parse_args(_matches: &clap::ArgMatches) -> Result<Self::ParsedArgs> {
-        // TODO
         Ok(())
     }
 }
